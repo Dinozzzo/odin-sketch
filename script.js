786e28322e524592fb0781eb6344dd9988a03ed6
Dino <dleootta@gmail.com>
2025-06-09 19:29:45 +0200

    Header created. Now need to create left and right div, and put container in right div.

--- script.js
+++ script.js
@@ -1,33 +1,59 @@
 const body = document.querySelector("body");
-const container = document.querySelector(".container");
-
 body.style.cssText = `
 background-color: rgb(244, 194, 194);`;
 
-container.style.cssText = `
+const header = document.createElement("div");
+header.style.cssText = `
+width: 100%;
+height: 150px;
 display: flex;
-flex-wrap: wrap;
-width: 640px;
-height: 640px;
-border: 5px solid rgb(128, 0, 32);
-background-color: rgb(244, 194, 194);`;
-
-for (let i = 0; i < 256; i++) {
-  const square = document.createElement("div");
-  square.style.cssText = `
-width: 40px;
-height: 40px;
+justify-content: center;
+align-items: center;
+font-size: 27.5px;
+text-transform: uppercase;
 `;
-  container.appendChild(square);
-
-  square.addEventListener("mouseover", () => {
-    square.style.backgroundColor = "red";
-  });
-}
+body.appendChild(header);
 
 const title = document.createElement("h1");
-body.prepend(title);
 title.textContent = `Etch-a-Sketch`;
-
 title.style.cssText = `
-color: rgb(128, 0, 32);`;
+color: rgb(128, 0, 32);
+`;
+header.appendChild(title);
+
+// const leftDiv = createElement("div");
+// const rightDiv = createElement("div");
+
+// leftDiv.style.cssText = `
+// width: 500px;`;
+
+// rightDiv.style.cssText = `
+// width: auto`;
+
+// body.appendChild(leftDiv);
+// body.appendChild(rightDiv);
+
+// rightDiv.appendChild(container);
+
+// const container = document.querySelector(".container");
+
+//container.style.cssText = `
+// display: flex;
+// flex-wrap: wrap;
+// width: 640px;
+// height: 640px;
+// border: 5px solid rgb(128, 0, 32);
+// background-color: rgb(244, 194, 194);`;
+
+// for (let i = 0; i < 256; i++) {
+//   const square = document.createElement("div");
+//   square.style.cssText = `
+// width: 40px;
+// height: 40px;
+// `;
+//   container.appendChild(square);
+
+//   square.addEventListener("mouseover", () => {
+//     square.style.backgroundColor = "red";
+//   });
+// }
